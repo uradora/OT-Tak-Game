ab13e9121e3fce225eb269cc9967ff8425d89ec1
Meri <meri.rajamaki@gmail.com>
2019-11-09 20:55:32 +0200

    added tests

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -7,14 +7,53 @@ import org.junit.Test;
 public class MaksukorttiTest {
 
     Maksukortti kortti;
+    Kassapaate kassapaate;
 
     @Before
     public void setUp() {
         kortti = new Maksukortti(10);
+        kassapaate = new Kassapaate();
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test 
+    public void kortinAlkusaldoOikein() {
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void lataaRahaa10EuroaOikein() {
+        kortti.lataaRahaa(1000);
+        assertEquals("saldo: 10.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinKunSyoMaukkaasti() {
+        kortti.lataaRahaa(1000);
+        kassapaate.syoEdullisesti(kortti);
+        assertEquals("saldo: 7.70", kortti.toString());
+    }
+    
+    @Test
+    public void josRahaaEiOleTarpeeksiSaldoEiMuutu() {
+        kassapaate.syoEdullisesti(kortti);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void palautaTrueJosRahatRiittivat() {
+        kortti.lataaRahaa(1000);
+        boolean rahaRiitti = kassapaate.syoEdullisesti(kortti);
+        assertTrue(rahaRiitti);
+    }
+    
+    @Test
+    public void palautaFalseJosRahatEivatRiittaneet() {
+        boolean rahaEiRiittanyt = kassapaate.syoEdullisesti(kortti);
+        assertFalse(rahaEiRiittanyt);
     }
 }
